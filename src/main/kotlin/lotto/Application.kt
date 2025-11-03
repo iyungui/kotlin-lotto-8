ea57ec563ce58932fadca11b21f7489c92c35cc9
iyungui <rsdbddml@gmail.com>
2025-11-03 14:46:52 +0900

    [feat](Rank.kt) enum 클래스로 Rank 클래스 생성

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -20,12 +20,18 @@ fun main() {
 
     println()
     println("${lottoCount}개를 구매했습니다.")
-    lottos.forEach { lotto ->
-        println(lotto)
-    }
+
     lottos.forEach { lotto ->
         val matchCount = lotto.countMatch(winningNumbers)
         val hasBonus = lotto.containsBonus(bonusNumber)
         println("$lotto - 일치: ${matchCount}개, 보너스: $hasBonus")
+
+        when (matchCount) {
+            6 -> println("-> ${Rank.FIRST}, 상금: ${Rank.FIRST.prize}원")
+            5 -> println("-> ${Rank.THIRD}, 상금: ${Rank.THIRD.prize}원")
+            4 -> println("-> ${Rank.FOURTH}, 상금: ${Rank.FOURTH.prize}원")
+            3 -> println("-> ${Rank.FIFTH}, 상금: ${Rank.FIFTH.prize}원")
+            else -> println("-> ${Rank.NONE}, 상금: ${Rank.NONE.prize}원")
+        }
     }
 }
