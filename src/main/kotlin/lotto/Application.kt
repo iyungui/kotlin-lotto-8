a732e2974c4c9c9f716d1ba55aff82aacd9e7ea3
iyungui <rsdbddml@gmail.com>
2025-11-03 13:49:09 +0900

    [feat](Application.kt) 구입 금액 입력 기능

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -1,5 +1,11 @@
 package lotto
 
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
-    // TODO: 프로그램 구현
+    println("구입금액을 입력해주세요")
+    val input = Console.readLine()
+    val amount = input.toInt()
+
+    println("입력한 금액: ${amount}원")
 }
