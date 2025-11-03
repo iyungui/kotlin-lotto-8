e1d4a35cf45f27869ae9a4371cd8f931c01c0844
iyungui <rsdbddml@gmail.com>
2025-11-03 14:11:47 +0900

    [feat](Application.kt) 보너스 번호 입력 및 검증 기능

--- src/main/kotlin/lotto/Application.kt
+++ src/main/kotlin/lotto/Application.kt
@@ -5,9 +5,10 @@ import camp.nextstep.edu.missionutils.Console
 fun main() {
     val amount = readPurchaseAmount()
     val numbers = readWinningNumbers()
+    val bonusNumber = readBonusNumber(numbers)
     println("입력한 금액: ${amount}원")
     println("입력한 당첨 번호: $numbers")
-
+    println("입력한 보너스 번호: $bonusNumber")
 }
 
 fun readPurchaseAmount(): Int {
@@ -62,5 +63,31 @@ fun validateWinningNumbers(numbers: List<Int>) {
     }
     if (numbers.size != numbers.toSet().size) {
         throw IllegalArgumentException("[ERROR] 당첨 번호는 중복될 수 없습니다.")
+    }
+}
+
+fun readBonusNumber(winningNumbers: List<Int>): Int {
+    while (true) {
+        try {
+            println("보너스 번호를 입력해 주세요.")
+            val input = Console.readLine()
+            val bonusNumber = input.toInt()
+
+            validateBonusNumber(bonusNumber, winningNumbers)
+            return bonusNumber
+        } catch (e: NumberFormatException) {
+            println("[ERROR] 보너스 번호는 숫자여야 합니다.")
+        } catch (e: IllegalArgumentException) {
+            println(e.message)
+        }
+    }
+}
+
+fun validateBonusNumber(bonusNumber: Int, winningNumbers: List<Int>) {
+    if (bonusNumber !in 1..45) {
+        throw IllegalArgumentException("[ERROR] 보너스 번호는 1부터 45 사이의 숫자여야 합니다.")
+    }
+    if (bonusNumber in winningNumbers) {
+        throw IllegalArgumentException("[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다.")
     }
 }
